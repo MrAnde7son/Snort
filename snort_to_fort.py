4efd9d570e85e500923a60a9d25e6b46b1ad1ffa
Itamar <mizrahi.itamar@gmail.com>
2017-07-06 18:21:52 +0300

    Add files via upload

--- snort_to_fort.py
+++ snort_to_fort.py
@@ -1,5 +1,4 @@
 import os.path,re,sys
-from idstools import rule
 import argparse
 
 
@@ -8,15 +7,16 @@ def SnortToFortigate(snort_rules):
     fort_rules = []
     try:
         if os.path.isfile(snort_rules):
-            rules += rule.parse_file(snort_rules)
+            rules += open(snort_rules,'r').readlines()
+        elif re.search('^alert\s+(tcp|udp|icmp|ip)\s+[^(]+\(.+\)$', snort_rules, re.IGNORECASE):
+            rules += [snort_rules]
         else:
-            rules += [rule.parse(snort_rules)]
+            sys.exit("Invalid rules input.")
     except:
         sys.exit("Error parsing snort rules. Please check the syntax.")
     for r in rules:
-        if not re.search('^alert\s+(tcp|udp|icmp|ip)\s+[^(]+\(.+\)$', str, re.IGNORECASE):
-            sys.exit("Invalid rule specified.\n" + r.raw + "\n")
-        raw = r.raw[r.raw.index('('):]
+        raw = r[r.index('('):]
+        raw = re.sub(r'\|[\s\w]+\|', lambda m: re.sub(r'\s*', '', m.group(0)), raw)
         raw = raw.replace('msg:', '--msg ')
         raw = raw.replace('flow:', '--flow ') if "flow" in raw else raw
         raw = raw.replace('pcre:', '--pcre ') if "pcre" in raw else raw
@@ -25,23 +25,23 @@ def SnortToFortigate(snort_rules):
         raw = raw.replace('flags:', '--tcp_flags ') if "flags" in raw else raw
         raw = raw.replace('distance:', '--distance ') if "distance" in raw else raw
         raw = raw.replace('within:', '--within ') if "within" in raw else raw
-        raw =re.sub(r'depth:\s*(\d+)', r'--distance \1,packet', raw) if "depth" in raw else raw
-        raw = re.sub(r'offset:\s*(\d+)', r'--within \1,packet', raw) if "within" in raw else raw
+        raw = re.sub(r'depth:\s*(\d+)', r'--distance \1,packet', raw) if "depth" in raw else raw
+        raw = re.sub(r'offset:\s*(\d+)', r'--within \1,packet', raw) if "offset" in raw else raw
         raw = raw.replace('nocase', '--nocase') if "nocase" in raw else raw
         raw = re.sub(r'detection_filter:\s*track\s*([^,]+)\s*,\s*count\s*(\d+)\s*,\s*seconds\s*(\d+)\s*',
                      r'--rate \2,\3; --track \1', raw) if "detection_filter" in raw else raw
         raw = re.sub(r'classtype[^;]+;\s?', '', raw)
         raw = re.sub(r'sid:\d+;\s?', '', raw)
-        header = r.header.split(" ")
+        header = r[:r.index('(')].split(" ")
         raw = re.sub(r'(msg[^;]+;\s?)', r'\1--protocol %s; ' % header[1].upper(), raw)
         if header[2] != 'any':
             raw = re.sub(r'(msg[^;]+;\s?)', r'\1--src_addr %s; ' % header[2], raw)
         if header[3] != 'any':
-            raw = re.sub(r'(msg[^;]+;\s?)', r'\1--src_port %s; ' % header[2], raw)
+            raw = re.sub(r'(msg[^;]+;\s?)', r'\1--src_port %s; ' % re.sub(r'[][]',r'',header[3]), raw)
         if header[5] != 'any':
             raw = re.sub(r'(msg[^;]+;\s?)', r'\1--dst_addr %s; ' % header[5], raw)
         if header[6] != 'any':
-            raw = re.sub(r'(msg[^;]+;\s?)', r'\1--dst_port %s; ' % header[6], raw)
+            raw = re.sub(r'(msg[^;]+;\s?)', r'\1--dst_port %s; ' % re.sub(r'[][]',r'',header[6]), raw)
         fort_rules += [raw]
 
     return fort_rules
@@ -63,7 +63,7 @@ def main():
     if args.output is not None:
         try:
             f = open(args.output,'w')
-            f.write("\n".join(list))
+            f.write("".join(list))
             f.close()
         except:
             sys.exit("Error opening %s." % args.output)
